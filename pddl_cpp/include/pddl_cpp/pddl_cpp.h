2fb129bc41a9f4598ac6f47b912979755e0a3d67
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2025-09-24 14:57:49 +0900

    compiled

--- pddl_cpp/include/pddl_cpp/pddl_cpp.h
+++ pddl_cpp/include/pddl_cpp/pddl_cpp.h
@@ -1,12 +1,72 @@
 #ifndef PDDL_CPP_PDDL_CPP_H
 #define PDDL_CPP_PDDL_CPP_H
 
-#include <pddl_cpp/domain.h>
-#include <pddl_cpp/problem.h>
-#include <pddl_cpp/step.h>
+#include <string>
+#include <vector>
+#include <memory>
+#include <actionlib/client/simple_action_client.h>
+#include <pddl_msgs/PDDLPlannerAction.h>
 
 namespace pddl_cpp {
 
+  class Action {
+  public:
+    std::string name;
+    std::string parameters;
+    std::string precondition;
+    std::string effect;
+    std::string on_condition;
+    std::string action_duration;
+  };
+
+  class Domain {
+  public:
+    std::string name;
+    std::string requirements;
+    std::vector<std::string> types;
+    std::vector<std::string> constants;
+    std::vector<std::string> predicates;
+    std::vector<Action> actions;
+    std::vector<std::string> functions;
+  };
+
+  class Object {
+  public:
+    std::string name;
+    std::string type;
+  };
+
+  class Problem {
+  public:
+    std::string name;
+    std::string domain;
+    std::vector<Object> objects;
+    std::vector<std::string> initial;
+    std::string goal;
+    std::string metric;
+  };
+
+  class Step {
+  public:
+    std::string action;
+    std::vector<std::string> args;
+    std::string start_time;
+    std::string action_duration;
+  };
+
+  class Planner {
+  public:
+    bool plan(const Domain& domain,
+              const Problem& problem,
+              double max_planning_time,
+              std::vector<Step>& sequence,
+              std::vector<std::string>& data,
+              bool use_durative_action);
+
+    std::string actionName;
+  protected:
+    std::shared_ptr<actionlib::SimpleActionClient<pddl_msgs::PDDLPlannerAction> > ac_;
+  };
 };
 
 #endif
--- pddl_cpp/src/pddl_cpp.cpp
+++ pddl_cpp/src/pddl_cpp.cpp
@@ -1,1 +1,91 @@
 #include <pddl_cpp/pddl_cpp.h>
+#include <ros/ros.h>
+
+namespace pddl_cpp {
+
+  bool Planner::plan(const Domain& domain,
+                     const Problem& problem,
+                     double max_planning_time,
+                     std::vector<Step>& sequence,
+                     std::vector<std::string>& data,
+                     bool use_durative_action) {
+
+    if(!ros::isInitialized()){
+      ros::init(ros::M_string(), "pddl_client");
+    }
+
+    if(!this->ac_){
+      this->ac_ = std::make_shared<actionlib::SimpleActionClient<pddl_msgs::PDDLPlannerAction> >("pddl_planner",true); // true causes the client to spin its own thread
+    }
+
+    this->ac_->waitForServer();
+
+    pddl_msgs::PDDLPlannerGoal goal;
+    goal.domain.name = domain.name;
+    goal.domain.requirements = domain.requirements;
+    goal.domain.types.resize(domain.types.size());
+    for(int i=0;i<domain.types.size();i++){
+      goal.domain.types[i] = domain.types[i];
+    }
+    goal.domain.constants.resize(domain.constants.size());
+    for(int i=0;i<domain.constants.size();i++){
+      goal.domain.constants[i] = domain.constants[i];
+    }
+    goal.domain.predicates.resize(domain.predicates.size());
+    for(int i=0;i<domain.predicates.size();i++){
+      goal.domain.predicates[i] = domain.predicates[i];
+    }
+    goal.domain.actions.resize(domain.actions.size());
+    for(int i=0;i<domain.actions.size();i++){
+      goal.domain.actions[i].name = domain.actions[i].name;
+      goal.domain.actions[i].parameters = domain.actions[i].parameters;
+      goal.domain.actions[i].precondition = domain.actions[i].precondition;
+      goal.domain.actions[i].effect = domain.actions[i].effect;
+      goal.domain.actions[i].on_condition = domain.actions[i].on_condition;
+      goal.domain.actions[i].action_duration = domain.actions[i].action_duration;
+    }
+    goal.domain.functions.resize(domain.functions.size());
+    for(int i=0;i<domain.functions.size();i++){
+      goal.domain.functions[i] = domain.functions[i];
+    }
+    goal.problem.name = problem.name;
+    goal.problem.domain = problem.domain;
+    goal.problem.objects.resize(problem.objects.size());
+    for(int i=0;i<problem.objects.size();i++){
+      goal.problem.objects[i].name = problem.objects[i].name;
+      goal.problem.objects[i].type = problem.objects[i].type;
+    }
+    goal.problem.initial.resize(problem.initial.size());
+    for(int i=0;i<problem.initial.size();i++){
+      goal.problem.initial[i] = problem.initial[i];
+    }
+    goal.problem.goal = problem.goal;
+    goal.problem.metric = problem.metric;
+    goal.max_planning_time = ros::Duration(max_planning_time);
+
+    actionlib::SimpleClientGoalState state = ac_->sendGoalAndWait(goal, ros::Duration(max_planning_time + 3.0));
+
+    if(state != actionlib::SimpleClientGoalState::SUCCEEDED){
+      return false;
+    }
+
+    pddl_msgs::PDDLPlannerResultConstPtr result = ac_->getResult();
+    sequence.resize(result->sequence.size());
+    for(int i=0;i<result->sequence.size();i++){
+      sequence[i].action = result->sequence[i].action;
+      for(int j=0;j<result->sequence[i].args.size();i++){
+        sequence[i].args[j] = result->sequence[i].args[j];
+      }
+      sequence[i].start_time = result->sequence[i].start_time;
+      sequence[i].action_duration = result->sequence[i].action_duration;
+    }
+    data.resize(result->data.size());
+    for(int i=0;i<result->data.size();i++){
+      data[i] = result->data[i];
+    }
+    use_durative_action = result->use_durative_action;
+
+    return true;
+  }
+
+};
